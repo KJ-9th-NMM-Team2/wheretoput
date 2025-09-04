a9acacdbea1d598651ea91b140c5768defee4230
송상록 <drinkingstraw33@gmail.com>
2025-09-04 15:51:50 +0900

    fix: 채팅 시 프사가 뜨게 하기

--- socket/src/chat/chat.service.ts
+++ socket/src/chat/chat.service.ts
@@ -181,6 +181,13 @@ export class ChatService {
           user_id: true,
           content: true,
           created_at: true,
+          User: {
+            select: {
+              id: true,
+              name: true,
+              image: true,
+            },
+          },
         },
       });
 
@@ -189,7 +196,8 @@ export class ChatService {
         id: r.message_id,
         roomId: r.chat_room_id,
         senderId: r.user_id,
-        senderName: '',
+        senderName: r.User?.name || '',
+        senderImage: r.User?.image || null,
         content: r.content,
         createdAt: r.created_at?.toISOString() ?? new Date().toISOString(),
         status: 'sent',
