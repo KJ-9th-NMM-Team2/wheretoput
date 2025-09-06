d2194a24719f5416ea49f953ac74f0d0c3373c46
likewoody <jsungj3@gmail.com>
2025-09-06 13:48:52 +0900

    feat: socket 3001번 포트 health api 구현

--- socket/src/app.module.ts
+++ socket/src/app.module.ts
@@ -6,9 +6,11 @@ import { RoomModule } from './room/room.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { CollabModule } from './collab/collab.module';
 import { RedisModule } from './redis/redis.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [PrismaModule, ChatModule, RoomModule, CollabModule, RedisModule],
+  imports: [PrismaModule, ChatModule, RoomModule, 
+    CollabModule, RedisModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
