b7307e47ba3def32d8b6f2ad421f7393da0edb3e
likewoody <jsungj3@gmail.com>
2025-09-13 01:08:48 +0900

    fix: API 문서 swagger 정리 및 CacheManager path 설정 에러 수정

--- next/app/api/achievement/[id]/route.ts
+++ next/app/api/achievement/[id]/route.ts
@@ -1,6 +1,49 @@
 import { checkAchievement } from "@/lib/api/achievement/checkAchievement";
 import { NextRequest } from "next/server";
 
+
+/**
+ * @swagger
+ * /api/achievement/{id}:
+ *   get:
+ *     tags:
+ *       - Achievement
+ *     summary: 특정 유저의 업적 조회
+ *     description: 사용자 ID를 이용해 해당 사용자의 업적을 조회합니다.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: 업적을 조회할 사용자 ID
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: 업적 조회 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   example: "123"
+ *                 achievements:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       name:
+ *                         type: string
+ *                         example: "First Chat"
+ *                       unlocked:
+ *                         type: boolean
+ *                         example: true
+ *       404:
+ *         description: 업적을 찾을 수 없음
+ *       500:
+ *         description: 서버 내부 오류
+ */
 export async function GET(req: NextRequest,
     { params }: { params: Promise<{ id: string }> }
 ) {
